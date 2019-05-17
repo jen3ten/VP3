b986d9195ceafa34a48cbc434a03ef7df622f23b
Sergio Rodriguez <rsergio98@yahoo.com>
2019-05-17 15:34:38 -0400

    first card

---
+++ Virtual Pets Amok/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Virtual_Pets_Amok
+{
+    class Menu
+    {
+        public string Goldfish { get; set; }
+        public string Moose { get; set; }
+        public string Ferret { get; set; }
+        public string Penguin { get; set; }
+
+        public Menu(string goldfish)
+        {
+            Goldfish = goldfish;
+        }
+
+
+   
+    }
+}
--- Virtual Pets Amok/Program.cs
+++ Virtual Pets Amok/Program.cs
@@ -6,8 +6,27 @@ namespace Virtual_Pets_Amok
     {
         static void Main(string[] args)
         {
+            bool toPlay = true;
+            Console.WriteLine("Welcome to VP3, type \"start\" to begin.\nType \"quit\" at any time to exit.");
+            string playResponse = Console.ReadLine().ToLower();
+        
+            if (playResponse.Equals("quit"))
+            {
+                toPlay = false;
+            }
+
+            while (toPlay) {
+                Console.WriteLine("Let's Play!");
+                
+                class Menu
+        {
+
+        }
+
+            }
             
-          
+            
+
 
         }
     }
